ed359f03698c5119d7f97d9327c6460893b6ff5d
arneesser <odenesser@gmail.com>
2025-10-23 22:28:09 +0200

    add adtl view

---
+++ Scripts/gold/create_view_top_reviews_monthly.sql
@@ -0,0 +1,16 @@
+drop view if exists  gold.v_top_reviews_month;
+
+CREATE OR REPLACE VIEW gold.v_top_reviews_month AS
+SELECT
+    p.brand,
+    t.year,
+    t.month,
+    t.month_name,
+    COUNT(*) AS total_reviews,
+    ROUND(AVG(f.rating)::numeric, 2) AS avg_rating
+FROM silver.fact_reviews f
+JOIN silver.dim_products p ON f.product_key = p.product_key
+JOIN silver.dim_time t ON f.time_key = t.time_key
+WHERE p.brand IS NOT NULL
+GROUP BY p.brand, t.year, t.month, t.month_name
+ORDER BY t.month, t.year, t.month_name, total_reviews DESC;
